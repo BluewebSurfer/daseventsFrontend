fc0ec38367a0fbc2675374137e2c474c3c521424
unknown <bluewebsurfer@gmail.com>
2021-03-16 12:16:43 +0530

    responsive in process

--- menu.js
+++ menu.js
@@ -1,8 +1,10 @@
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+  console.log("hello");
+    const x = document.getElementById('myTopnav');
+    console.log(x);
+    // if (x.className == "topnav") {
+    //   x.className += " responsive";
+    // } else {
+    //   x.className = "topnav";
+    // }
   }
