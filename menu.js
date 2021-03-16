b8c9f9611293004ba7f720d08c007ef32700c9ac
unknown <bluewebsurfer@gmail.com>
2021-03-16 18:08:47 +0530

    responsive done

--- menu.js
+++ menu.js
@@ -1,10 +1,8 @@
 function myFunction() {
-  console.log("hello");
-    const x = document.getElementById('myTopnav');
-    console.log(x);
-    // if (x.className == "topnav") {
-    //   x.className += " responsive";
-    // } else {
-    //   x.className = "topnav";
-    // }
+    var x = document.getElementById("myId");
+    if (x.className === "mynav") {
+      x.className += " responsive";
+    } else {
+      x.className = "mynav";
+    }
   }
