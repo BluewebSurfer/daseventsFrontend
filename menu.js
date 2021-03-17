8dd4a91cc0370a1d49161f753da929dae5fb3f2f
unknown <bluewebsurfer@gmail.com>
2021-03-17 10:42:59 +0530

    file structure changed

--- menu.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("myId");
-    if (x.className === "mynav") {
-      x.className += " responsive";
-    } else {
-      x.className = "mynav";
-    }
-  }
